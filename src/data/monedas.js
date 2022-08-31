44eabbca26e021aee020110a29652ec72e3d0532
lizeth_urrego <lizethnup@gmail.com>
2022-08-31 16:21:35 -0500

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+    {id:'USD', nombre: 'Dolar Estadunidense'},
+    {id:'MXN', nombre: 'Peso Mexicano'},
+    {id:'EUR', nombre: 'Euro'},
+    {id:'GPB', nombre: 'Libra ESterkina'},
+]
+
+export {monedas}
